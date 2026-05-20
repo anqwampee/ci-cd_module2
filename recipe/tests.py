1ae2b2d32b15558c86add9083d90a91387dbc8cc
Anastasiia Pudova <anastasiia.pudova@gmail.com>
2026-05-20 14:30:52 +0300

    add unit tests

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,60 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Category, Recipe
 
-# Create your tests here.
+
+class MainViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.category = Category.objects.create(name='Desserts')
+        for i in range(15):
+            Recipe.objects.create(
+                title=f'Recipe {i}',
+                description=f'Description {i}',
+                instructions=f'Instructions {i}',
+                ingredients=f'Ingredients {i}',
+                category=self.category
+            )
+
+    def test_main_view_status_code(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_view_template(self):
+        response = self.client.get(reverse('main'))
+        self.assertTemplateUsed(response, 'main.html')
+
+    def test_main_view_returns_max_10_recipes(self):
+        response = self.client.get(reverse('main'))
+        self.assertLessEqual(len(response.context['recipes']), 10)
+
+
+class CategoryDetailViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.category = Category.objects.create(name='Soups')
+        self.recipe = Recipe.objects.create(
+            title='Borsch',
+            description='Ukrainian soup',
+            instructions='Cook it',
+            ingredients='Beets, cabbage',
+            category=self.category
+        )
+
+    def test_category_detail_status_code(self):
+        response = self.client.get(reverse('category_detail', kwargs={'pk': self.category.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_detail_template(self):
+        response = self.client.get(reverse('category_detail', kwargs={'pk': self.category.pk}))
+        self.assertTemplateUsed(response, 'category_detail.html')
+
+    def test_category_detail_shows_correct_recipes(self):
+        response = self.client.get(reverse('category_detail', kwargs={'pk': self.category.pk}))
+        self.assertIn(self.recipe, response.context['recipes'])
+
+    def test_category_detail_404(self):
+        response = self.client.get(reverse('category_detail', kwargs={'pk': 9999}))
+        self.assertEqual(response.status_code, 404)
